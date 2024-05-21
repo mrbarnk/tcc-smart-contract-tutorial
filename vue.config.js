2905bc218a2d67184c9403304e37df2d652a2379
Bankole Emmanuel <mrbarnk1@gmail.com>
2024-05-21 17:22:10 +0100

    added gitlab ci

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
+  process.env.NODE_ENV === "production" ? "/tcc-smart-contract-tutorial/" : "/",
   transpileDependencies: true
 })
